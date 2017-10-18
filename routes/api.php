919207bf68faf1ce93c171384ae2320b74fe335e
liyingxuan <mvp_xuan@163.com>
2017-10-18 12:31:40 +0800

    增加获取单条数据的方法

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,19 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 // 建立一个给vuejs-cli-tutorial项目提供api数据的
 // 中间件cors是解决跨域访问问题的
 Route::middleware('cors:api')->get('/todos', function () {
-    return response()->json([
+    $todos = [
         ['id' => 1, 'title' => 'Learn VueJS', 'completed' => false],
         ['id' => 2, 'title' => 'Coding', 'completed' => true]
-    ]);
+    ];
+
+    return response()->json($todos); // 测试使用，故没有直接使用DB
+});
+
+Route::middleware('cors:api')->get('/todo/{id}', function ($id) {
+    $todos = [
+        ['id' => 1, 'title' => 'Learn VueJS', 'completed' => false],
+        ['id' => 2, 'title' => 'Coding', 'completed' => true]
+    ];
+
+    return response()->json($todos[$id - 1]); // 测试使用，故没有直接使用DB
 });
